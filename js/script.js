2bd35a978310925d25bbbad4d39e3cd4c410cea6
Stanley Su <stanleysu00@gmail.com>
2017-07-30 21:39:08 +1000

    cached element selectors and added break adjustment functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+(function() {
+	// html elements
+	const breakDiv = document.getElementById('break'),
+		  sessionDiv = document.getElementById('session'),
+		  breakValue = document.getElementById('breakValue'),
+		  sessionValue = document.getElementById('sessionValue'),
+		  timeDiv = document.getElementById('time'),
+		  animationDiv = document.getElementById('animation');
+
+	// functions
+	function getTime() {
+		return parseInt(timeDiv.textContent);
+	}
+
+	// event listeners
+
+	// adjust break length
+	breakDiv.addEventListener('click', function(e) {
+		let target = e.target,
+			id = target.id;
+
+		// alter break length depending on button clicked
+		if (id === 'minusBreak') {
+			--breakValue.textContent;
+		} else if (id === 'plusBreak') {
+			++breakValue.textContent;
+		}
+	})
+})();
+
