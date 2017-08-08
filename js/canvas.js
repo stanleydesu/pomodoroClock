8c55ecb8399b4bad6a4ff32c85a1125cf60450f1
Stanley Su <stanleysu00@gmail.com>
2017-08-08 11:33:02 +1000

    updated svg path for volcano

--- js/canvas.js
+++ js/canvas.js
@@ -9,7 +9,7 @@ canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
 function drawVolcano() {
-	const volcano = new Path2D('M 30 340 L 70 260 L 110 260 L 130 300 L 110 260 L 90 260 L 130 180 L 170 180 L 190 220 L 180 240 L 170 240 L 160 230 L 140 230 L 130 240 L 120 240 L 110 220 L 130 180 L 170 180 L 190 220 L 220 280 L 200 280 L 180 320 L 200 280 L 220 280 L 240 280 L 270 340');
+	const volcano = new Path2D('M 30 340 L 70 260 L 110 260 L 130 300 L 110 260 L 90 260 L 130 180 L 170 180 L 190 220 L 180 240 L 165 240 L 160 230 L 140 230 L 135 240 L 120 240 L 110 220 L 130 180 L 170 180 L 190 220 L 220 280 L 200 280 L 180 320 L 200 280 L 220 280 L 240 280 L 270 340');
 	c.strokeStyle = 'white';
 	c.lineWidth = '5';
 	c.stroke(volcano);
